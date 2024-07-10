6edf41ecdc11e376928b4f7993faa71f946f2f96
hafizurrahmanomar <hafizurrahmanomar@gmail.com>
2024-07-10 14:34:39 +0600

    Counter app update

--- counter-app/src/App.jsx
+++ counter-app/src/App.jsx
@@ -1,8 +1,23 @@
+/* eslint-disable no-unused-vars */
 import { useState } from 'react';
 import './App.css';
 
+
 function App() {
-  const [count,setCount]=useState(0);
+  
+  return(
+    <div className="App">
+      <div>
+    <Counter />
+      </div>
+    </div>
+  );
+}
+
+
+function Counter() {
+  
+  const [count,setCount] = useState(0);
 
   const increaseCount =()=> {
     setCount(count+1);
@@ -19,13 +34,51 @@ function App() {
   const resetCount =()=> setCount(0);
 
   return (
-    <div className="App">
+   <div>
     <div><h1>Count is = {count}</h1></div>
-    <button onClick={increaseCount}> + </button>
-    <button onClick={decreaseCount}>-</button>
-    <button onClick={resetCount}><h1>Reset</h1></button>
+    <button onClick={increaseCount}> Increase(+)</button>
+    <br/>
+    <br/>
+    <button onClick={decreaseCount}>Decrease(-)</button>
+    <br/>
+    <br/>
+    <button onClick={resetCount}>Reset</button>
     </div>
   );
 }
+
+// Other Way
+
+// function Counter() {
+  
+//   //const [count,setCount] = useState(0);
+//   const [count,setCount] = useState(0);
+//   const decreaseCount =()=> {
+//     if(count===0){
+//       alert('Count Can not less than zero');
+//       return
+//     }
+//     setCount(count-1);
+//   };
+
+//   const resetCount =()=> setCount(0);
+
+//   return (
+//    <div>
+//     <div><h1>Count is = {count}</h1></div>
+//     <button onClick={()=>setCount(count+1)}> Increase(+)</button>
+//     <br/>
+//     <button onClick={()=> {
+//     if(count===0){
+//       alert('Count Can not less than zero');
+//       return
+//     }
+//     setCount(count-1);
+//   }}>Decrease(-)</button>
+//     <br/>
+//     <button onClick={()=> setCount(0)}>Reset</button>
+//     </div>
+//   );
+// }
 
 export default App;
